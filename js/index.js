1c5f6ae7347ff06a3793377dbae2fe7c0d98f5d6
habin-isa <habin@hotmail.co.uk>
2019-02-04 18:38:45 +0000

    adds url and string validation for label

--- js/index.js
+++ js/index.js
@@ -23,14 +23,40 @@ function appCtrl ($scope) {
       url: 'http://google.com',
       category: 'Web Design',
       likes: 15
+    },
+    {
+      title: 'Google',
+      url: 'http://google.com',
+      category: 'Web Design',
+      likes: 15
+    },
+    {
+      title: 'Google',
+      url: 'http://google.com',
+      category: 'Web Design',
+      likes: 15
     }
   ];
     
   $scope.increment = function ($index) {
     $scope.likes++;
-   };
+  };
+
+  $scope.validUrl = function (str) {
+    regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+      if (regexp.test(str))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  };
   
   $scope.addBookmark = function () {
+    $scope.validUrl($scope.site_url);
 	  $scope.bookmarks.push({
 		  title: $scope.site_name,
 		  url: $scope.site_url,
@@ -39,7 +65,8 @@ function appCtrl ($scope) {
 	  });
 	  $scope.site_name = '';
 	  $scope.site_url = '';
-	  $scope.site_category = '';
+    $scope.site_category = '';
+    window.location.href = "submission.html";
   },
     
   $scope.deleteBookmark = function (id) {

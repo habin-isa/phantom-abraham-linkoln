c32a4b0a791c74ba0ab796a1f626cec12585f9f5
habin-isa <habin@hotmail.co.uk>
2019-02-05 14:19:42 +0000

    first app deploy

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
